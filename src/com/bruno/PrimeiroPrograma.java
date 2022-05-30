2b8cd4534c9b3a646b644a726fc5dea47736c09f
Bruno Gusmão <brunogusmao43@gmail.com>
2022-05-30 16:24:26 -0300

    first commit

---
+++ src/com/bruno/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package com.bruno;
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		char[] gato = null;
+		System.out.println(gato);
+
+	}
+
+}
